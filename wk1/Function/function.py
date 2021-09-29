96a7d75dd086c8ac67f52f645047b78686825c43
bryan-huffstutler <bryan.huffstutler@bryanuniversity.edu>
2021-09-29 18:20:53 -0500

    Perfect Number

--- wk1/Function/function.py
+++ wk1/Function/function.py
@@ -1,20 +1,22 @@
+def perfectNumber() :
+  # Get Number From User
+  n = int(input("Enter any number: "))
 
-# Get Number From User
-n = int(input("Enter any number: "))
+  # Setup Empty Variable
+  sum1 = 0
 
-# Setup Empty Variable
-sum1 = 0
+  # For Loop to Check Numbers
+  # This for in range checks all numbers between 1 and n
+  for i in range(1, n):
+    # Checks if n modulus i = 0 or not, To see if it's divisible or not
+      if(n % i == 0):
+        # Adds i to sum1 until sum1 == n
+          sum1 = sum1 + i
+  # After for loop completes, if sum1 == n, then the number is perfect
+  if (sum1 == n):
+      print("The number is a Perfect number!")
+  # Otherwise it is not.
+  else:
+      print("The number is not a Perfect number!")
 
-# For Loop to Check Numbers
-# This for in range checks all numbers between 1 and n
-for i in range(1, n):
-  # Checks if n modulus i = 0 or not, To see if it's divisible or not
-    if(n % i == 0):
-      # Adds i to sum1 until sum1 == n
-        sum1 = sum1 + i
-# After for loop completes, if sum1 == n, then the number is perfect
-if (sum1 == n):
-    print("The number is a Perfect number!")
-# Otherwise it is not.
-else:
-    print("The number is not a Perfect number!")
+perfectNumber()
